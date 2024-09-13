2e35369857fac4618cd3cfd04f73ed94927c8afa
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2024-09-13 20:54:21 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // Using the baseurl to make requests from backend
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://memory-game-backend.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
